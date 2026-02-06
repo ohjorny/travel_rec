4d0f2caf0fc1022413912f85e48f3b5fb1c8ce41
Jon <jpaperny97@outlook.com>
2026-02-06 00:31:39 -0500

    adding style to about page and begin implementing js to list team members

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -14,6 +14,37 @@ function toggleSearchBar() {
     })
 }
 
+const members = [
+    {
+        img: "",
+        name: "John Doe",
+        description: "John is a fantastic CEO",
+        title: "CEO"
+    },
+    {
+        img: "./ronald.jpg",
+        name: "Ronald McDonald",
+        description: "Ronald puts a smile on everyone's face!",
+        title: "Mascot"
+    },
+    {
+        img: "./jake.jpg",
+        name: "Jake Gyllenhaal",
+        description: "Responsible for spreading our image and product throughout the world, Mr. Gyllenhaal does a fantastic job of promoting Traveloria!",
+        title: "Ambassador"
+    }
+]
+
+const memberList = document.getElementById("member-list");
+
+function showMembers() {
+
+    for (const mem of members) {
+        const newImg = document.createElement("img");
+        const newMember = document.createElement("div");
+    }
+}
+
 toggleSearchBar();
 
 window.addEventListener("hashchange", toggleSearchBar);
